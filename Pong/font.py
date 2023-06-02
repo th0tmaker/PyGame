3103ca58c1c1d1ac83a4cf32fc06e58237d3a31d
th0tmaker <114862399+th0tmaker@users.noreply.github.com>
2023-06-02 10:39:51 +0200

    Update font.py
    Change hardcoded font paths

--- Pong/font.py
+++ Pong/font.py
@@ -4,9 +4,9 @@ from setup import pg, setup_pygame
 setup_pygame()  # initialize pygame module to access pg.font.Font
 
 # font (paths)
-menu_font = "C:/Users/krasn/PycharmProjects/My_Projects/PyGame/Pong/font/Menu.ttf"
-score_font = "C:/Users/krasn/PycharmProjects/My_Projects/PyGame/Pong/font/Score.ttf"
-countdown321_font = "C:/Users/krasn/PycharmProjects/My_Projects/PyGame/Pong/font/Countdown321.ttf"
+menu_font = "Pong/font/Menu.ttf"
+score_font = "Pong/font/Score.ttf"
+countdown321_font = "Pong/font/Countdown321.ttf"
 
 # texts
 menu_txt = pg.font.Font(menu_font, 22)
